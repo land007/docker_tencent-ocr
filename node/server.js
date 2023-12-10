b119997c47fdd25b2ac4bc5174b3f8c30d0089b3
阿七 <yiqiujia@hotmail.com>
2023-12-10 19:41:07 +0800

    只让上传PNG、JPG、JPEG、BMP、PDF格式

--- node/server.js
+++ node/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json({ limit: '7mb' }));
 app.use(bodyParser.urlencoded({ limit: '7mb', extended: true }));
 // 允许所有域进行跨域访问
 const corsOptions = {
-  origin: '*',
+	origin: '*',
 };
 app.use(cors(corsOptions));
 
@@ -19,36 +19,40 @@ app.use(cors(corsOptions));
 app.use(express.static(path.join(__dirname, "public")));
 
 const clientConfig = {
-  credential: {
-    secretId: process.env.TENCENT_CLOUD_SECRET_ID,
-    secretKey: process.env.TENCENT_CLOUD_SECRET_KEY,
-  },
-  region: process.env.TENCENT_CLOUD_REGION || "ap-beijing",
-  profile: {
-    httpProfile: {
-      endpoint: "ocr.tencentcloudapi.com",
-    },
-  },
+	credential: {
+		secretId: process.env.TENCENT_CLOUD_SECRET_ID,
+		secretKey: process.env.TENCENT_CLOUD_SECRET_KEY,
+	},
+	region: process.env.TENCENT_CLOUD_REGION || "ap-beijing",
+	profile: {
+		httpProfile: {
+			endpoint: "ocr.tencentcloudapi.com",
+		},
+	},
 };
 
 const ocrClient = new tencentcloud.ocr.v20181119.Client(clientConfig);
 
 app.post("/ocr", async (req, res) => {
-  try {
-    const { imageBase64 } = req.body;
+	try {
+		const { imageBase64 } = req.body;
 
-    const params = {
-      ImageBase64: imageBase64,
-    };
+		const params = {
+			ImageBase64: imageBase64,
+			"IsPdf": true,
+			//"LanguageType": "auto",
+			//"PdfPageNumber": 1,
+			//"IsWords": false
+		};
 
-    const data = await ocrClient.GeneralAccurateOCR(params);
-    res.json(data);
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+		const data = await ocrClient.GeneralAccurateOCR(params);
+		res.json(data);
+	} catch (error) {
+		console.error("Error:", error);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
